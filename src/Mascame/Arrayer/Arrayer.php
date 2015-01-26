4fa771799c141afdeb67aa1184a0beeab82d4e83
Marc Mascarell <marcmascarell@gmail.com>
2015-01-27 00:38:00 +0100

    refactoring. Added some useful array manipulation methods

--- src/Mascame/Arrayer/Arrayer.php
+++ src/Mascame/Arrayer/Arrayer.php
@@ -6,131 +6,22 @@ namespace Mascame\Arrayer;
 class Arrayer {
 
     /**
+     * @var array
+     */
+    protected $array;
+
+    /**
      * @var
      */
-    protected $content;
+    protected $arrayDot = array();
 
     /**
-     * @var int
+     * @param array $array
      */
-    protected $indentation = 0;
-
-
-    /**
-     * @param array $values
-     */
-    public function __construct(array $values) {
-        return $this->make($values);
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent() {
-        return $this->start() . $this->content . $this->end();
-    }
-
-    /**
-     * @param $values
-     * @param null $key
-     * @param string $indentation
-     * @return $this
-     */
-    protected function make($values, $key = null, $indentation = 'initial')
-    {
-        if ($indentation == 'initial' && !$key) {
-            $this->indentation = 0;
-        } elseif ($key !== null) {
-            $this->indentation++;
-        }
-
-        if ($key !== null) $this->content .= $this->keyStart($key);
-
-        if (is_array($values)) {
-            $this->content .= $this->add($values);
-        } else {
-            $this->content .= $values;
-        }
-
-        if ($key !== null) $this->content .= $this->keyEnd();
-
-        if ($indentation != 'initial' && $key !== null) {
-            $this->indentation--;
-        }
+    public function __construct(array $array) {
+        $this->array = $array;
 
         return $this;
-    }
-
-    /**
-     * @return string
-     */
-    protected function start()
-    {
-        $content = '<?php' . PHP_EOL . PHP_EOL;
-        $content .= 'return array(' . PHP_EOL . PHP_EOL;
-
-        return $content;
-    }
-
-    /**
-     * @param $key
-     * @return string
-     */
-    protected function keyStart($key)
-    {
-        return $this->getIndentationTabs() . $this->wrapElement($key) . ' => array(' . PHP_EOL;
-    }
-
-    /**
-     * @return string
-     */
-    protected function keyEnd()
-    {
-        return $this->getIndentationTabs() . '),' . PHP_EOL . PHP_EOL;
-    }
-
-    /**
-     * @param $array
-     * @return string
-     */
-    protected function add($array)
-    {
-        $content = '';
-
-        foreach ($array as $key => $value) {
-            if (!is_array($value)) {
-                $content .= $this->getIndentationTabs(1) . $this->wrapElement($key) . ' => ' . $this->wrapElement($value) . ',' . PHP_EOL;
-            } else {
-                $this->make($value, $key, 'indent');
-            }
-
-        }
-
-        return $content;
-    }
-
-    /**
-     * @param int $plus
-     * @return string
-     */
-    protected function getIndentationTabs($plus = 0) {
-        return str_repeat("\t", $this->indentation + $plus);
-    }
-
-    /**
-     * @param $string
-     * @return string
-     */
-    private function wrapElement($string) {
-        return '"' . addslashes($string) . '"';
-    }
-
-    /**
-     * @return string
-     */
-    protected function end()
-    {
-        return ');';
     }
 
     /**
@@ -138,6 +29,180 @@ class Arrayer {
      * @return string
      */
     public function append($array) {
-        return $this->make($array);
+        array_push($this->array, $array);
+
+        $this->convertToArrayDot();
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     */
+    public function set($key, $value) {
+        $this->arraySet($this->array, $key, $value);
+
+        $this->convertToArrayDot();
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getArray() {
+        return $this->array;
+    }
+
+    /**
+     * @param $key
+     */
+    public function delete($key) {
+        $arrayDot = $this->getArrayDot();
+
+        unset($arrayDot[$key]);
+
+        $this->revertArrayDot($arrayDot);
+
+        return $this;
+    }
+
+    /**
+     * Flatten a multi-dimensional associative array with dots.
+     * From Laravel framework
+     *
+     * @param  array   $array
+     * @param  string  $prepend
+     * @return array
+     */
+    protected function arrayDot($array, $prepend = null) {
+        if (function_exists('array_dot')) return array_dot($array);
+
+        $results = array();
+
+        foreach ($array as $key => $value)
+        {
+            if (is_array($value))
+            {
+                $results = array_merge($results, $this->arrayDot($value, $prepend.$key.'.'));
+            }
+            else
+            {
+                $results[$prepend.$key] = $value;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * @param array $arrayDot
+     */
+    protected function revertArrayDot($arrayDot = array()) {
+        $array = (!empty($arrayDot)) ? $arrayDot : $this->arrayDot;
+        $this->array = array();
+
+        foreach ($array as $key => $value) {
+            $this->arraySet($this->array, $key, $value);
+        }
+    }
+
+    /**
+     * @param array $array
+     * @return array
+     */
+    protected function convertToArrayDot($array = array()) {
+        if (!empty($array)) {
+            return $this->arrayDot($array);
+        }
+
+        return $this->arrayDot = $this->arrayDot($this->array);
+    }
+
+    /**
+     * @return array
+     */
+    public function getArrayDot() {
+        if (!empty($this->arrayDot)) return $this->arrayDot;
+
+        return $this->convertToArrayDot();
+    }
+
+    /**
+     * @param $key
+     * @param null $default
+     * @return mixed
+     */
+    public function get($key, $default = null) {
+        return $this->arrayGet($this->arrayDot, $key, $default);
+    }
+
+    /**
+     * Set an array item to a given value using "dot" notation.
+     *
+     * If no key is given to the method, the entire array will be replaced.
+     *
+     * From Laravel framework
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $value
+     * @return array
+     */
+    protected function arraySet(&$array, $key, $value) {
+        if (function_exists('array_set')) return array_set($array, $key, $value);
+
+        if (is_null($key)) return $array = $value;
+
+        $keys = explode('.', $key);
+
+        while (count($keys) > 1)
+        {
+            $key = array_shift($keys);
+
+            // If the key doesn't exist at this depth, we will just create an empty array
+            // to hold the next value, allowing us to create the arrays to hold final
+            // values at the correct depth. Then we'll keep digging into the array.
+            if ( ! isset($array[$key]) || ! is_array($array[$key]))
+            {
+                $array[$key] = array();
+            }
+
+            $array =& $array[$key];
+        }
+
+        $array[array_shift($keys)] = $value;
+
+        return $array;
+    }
+
+    /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    protected function arrayGet($array, $key, $default = null)
+    {
+        if (function_exists('array_get')) return array_get($array, $key, $default = null);
+
+        if (is_null($key)) return $array;
+
+        if (isset($array[$key])) return $array[$key];
+
+        foreach (explode('.', $key) as $segment)
+        {
+            if ( ! is_array($array) || ! array_key_exists($segment, $array))
+            {
+                return ($default instanceof \Closure)  ? $default() : $default;
+            }
+
+            $array = $array[$segment];
+        }
+
+        return $array;
     }
 }
---
+++ src/Mascame/Arrayer/Builder.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace Mascame\Arrayer;
+
+
+class Builder extends Arrayer {
+
+    /**
+     * @var
+     */
+    protected $content;
+
+    /**
+     * @var int
+     */
+    protected $indentation = 0;
+
+    /**
+     * @var \Mascame\Arrayer\Arrayer
+     */
+    protected $arrayer;
+
+
+    /**
+     * @var bool
+     */
+    protected $minified;
+
+
+    /**
+     * @param array $array
+     */
+    public function __construct(array $array, $minified = false) {
+        $this->array = $array;
+        $this->minified = $minified;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent() {
+        $this->make($this->getArray());
+
+        return $this->start() . $this->content . $this->end();
+    }
+
+
+    /**
+     * @param $values
+     * @param null $key
+     * @param string $indentation
+     * @return $this
+     */
+    protected function make($values, $key = null, $indentation = 'initial')
+    {
+        if ($indentation == 'initial' && !$key) {
+            $this->indentation = 0;
+        } elseif ($key !== null) {
+            $this->indentation++;
+        }
+
+        if ($key !== null) $this->content .= $this->keyStart($key);
+
+        if (is_array($values)) {
+            $this->content .= $this->add($values);
+        } else {
+            $this->content .= $values;
+        }
+
+        if ($key !== null) $this->content .= $this->keyEnd();
+
+        if ($indentation != 'initial' && $key !== null) {
+            $this->indentation--;
+        }
+
+        return $this->content;
+    }
+
+    /**
+     * @param $array
+     * @return string
+     */
+    protected function add($array)
+    {
+        $content = '';
+
+        foreach ($array as $key => $value) {
+
+            if (!is_array($value)) {
+                $content .= $this->getIndentationTabs(1)
+                        . $this->wrapElement($key)
+                        . $this->keyValueSeparator()
+                        . $this->wrapElement($value) . ','
+                        . $this->addSeparation(null);
+            } else {
+                $this->make($value, $key, 'indent');
+            }
+
+        }
+
+        return $content;
+    }
+
+    /**
+     * @param int $plus
+     * @return string
+     */
+    protected function getIndentationTabs($plus = 0) {
+        if ($this->minified) return null;
+
+        return str_repeat("\t", $this->indentation + $plus);
+    }
+
+    /**
+     * @param $string
+     * @return string
+     */
+    private function wrapElement($string) {
+        return '"' . addslashes($string) . '"';
+    }
+
+    /**
+     * @return string
+     */
+    protected function end()
+    {
+        return ');';
+    }
+
+    /**
+     * @return string
+     */
+    protected function start()
+    {
+        $content = '<?php' . $this->doubleSeparation();
+        $content .= 'return array(' . $this->doubleSeparation(null);
+
+        return $content;
+    }
+
+    /**
+     * @param $key
+     * @return string
+     */
+    protected function keyStart($key)
+    {
+        return $this->getIndentationTabs()
+            . $this->wrapElement($key)
+            . $this->customSeparation('=>array(', ' => array(')
+            . $this->addSeparation(null);
+    }
+
+    /**
+     * @return string
+     */
+    protected function keyEnd()
+    {
+        return $this->getIndentationTabs() . '),' . $this->doubleSeparation(null);
+    }
+
+    /**
+     * @param string $separator
+     * @return string
+     */
+    protected function addSeparation($separator = ' ') {
+        return ($this->minified) ? $separator : PHP_EOL;
+    }
+
+    /**
+     * @param string $separator
+     * @return string
+     */
+    protected function doubleSeparation($separator = ' ') {
+        return ($this->minified) ? $separator : $this->addSeparation() . $this->addSeparation();
+    }
+
+    /**
+     * @param $minified
+     * @param $normal
+     */
+    protected function customSeparation($minified, $normal) {
+        return ($this->minified) ? $minified : $normal;
+    }
+
+    /**
+     * @return string
+     */
+    protected function keyValueSeparator() {
+        return ($this->minified) ? '=>' : ' => ';
+    }
+}
